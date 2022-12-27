6541a0ed07191596bf513ceefd2ef3b26fd2e5b4
Slogllykop <slogllykop07@gmail.com>
2022-12-28 03:59:32 +0530

    function template

---
+++ practice/LA5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#define size 10
+
+template <class T> void selectionSort(T arr[size], int length) {
+    for(int i = 0; i < length - 1; i++) {
+        int min = i;
+        for(int j = i + 1; j < length; j++) {
+            if (arr[j] < arr[min]) min = j;
+        }
+        std::swap(arr[i], arr[min]);
+    }
+}
+
+int main() {
+    int A[size]; float B[size];
+    int choice, n;
+    std::cout << "Enter the number of elements: "; std::cin >> n;
+    std::cout << "Which type of number?\n1] int\n2] float\nEnter your choice: "; std::cin >> choice;
+    switch (choice)
+    {
+        case 1:
+            for(int i = 0; i < n; i++) std::cin >> A[i];
+            selectionSort(A, n);
+            for(int i = 0; i < n; i++) std::cout << A[i] << " ";
+            break;
+        
+        case 2:
+            for(int i = 0; i < n; i++) std::cin >> B[i];
+            selectionSort(B, n);
+            for(int i = 0; i < n; i++) std::cout << B[i] << " ";
+            break;
+
+        default: break;
+    }
+    return 0;
+}
